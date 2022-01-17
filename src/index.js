e2324b9bb8035b7cfad154dda71ec9f400f7bd66
isAdrisal <29012873+isAdrisal@users.noreply.github.com>
2022-01-17 21:54:54 +1100

    Change all class methods to private methods

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ export default customElements.define(
       const valueAsNumber = Number(value);
       this._min = valueAsNumber;
       this.setAttribute('min', valueAsNumber);
-      this._setInputProperty(this._minInput, 'min', valueAsNumber);
-      this._setInputProperty(this._maxInput, 'min', valueAsNumber);
+      this.#setInputProperty(this._minInput, 'min', valueAsNumber);
+      this.#setInputProperty(this._maxInput, 'min', valueAsNumber);
     }
 
     get max() {
@@ -27,8 +27,8 @@ export default customElements.define(
       const valueAsNumber = Number(value);
       this._max = valueAsNumber;
       this.setAttribute('max', valueAsNumber);
-      this._setInputProperty(this._minInput, 'max', valueAsNumber);
-      this._setInputProperty(this._maxInput, 'max', valueAsNumber);
+      this.#setInputProperty(this._minInput, 'max', valueAsNumber);
+      this.#setInputProperty(this._maxInput, 'max', valueAsNumber);
     }
 
     get step() {
@@ -38,8 +38,8 @@ export default customElements.define(
     set step(value) {
       this._step = value;
       this.setAttribute('step', value);
-      this._setInputProperty(this._minInput, 'step', value);
-      this._setInputProperty(this._maxInput, 'step', value);
+      this.#setInputProperty(this._minInput, 'step', value);
+      this.#setInputProperty(this._maxInput, 'step', value);
     }
 
     get disabled() {
@@ -73,7 +73,7 @@ export default customElements.define(
       }
       this._valueMin = valueAsNumber;
       this.setAttribute('valueMin', valueAsNumber);
-      this._setInputProperty(this._minInput, 'value', valueAsNumber);
+      this.#setInputProperty(this._minInput, 'value', valueAsNumber);
       this.midpoint = (this.valueMax - this.valueMin) / 2 + this.valueMin;
 
       if (this.ready) {
@@ -103,7 +103,7 @@ export default customElements.define(
       }
       this._valueMax = valueAsNumber;
       this.setAttribute('valueMax', valueAsNumber);
-      this._setInputProperty(this._maxInput, 'value', valueAsNumber);
+      this.#setInputProperty(this._maxInput, 'value', valueAsNumber);
       this.midpoint = (this.valueMax - this.valueMin) / 2 + this.valueMin;
 
       if (this.ready) {
@@ -142,155 +142,11 @@ export default customElements.define(
       style.innerHTML = compiledStyles;
       shadowRoot.appendChild(style);
       shadowRoot.appendChild(template.content.cloneNode(true));
-
-      this._init = () => {
-        this._container = this.shadowRoot.querySelector('[part="container"]');
-        this._minInput = this.shadowRoot.querySelector('[data-input="min"]');
-        this._maxInput = this.shadowRoot.querySelector('[data-input="max"]');
-        this._inputs = [this._minInput, this._maxInput];
-
-        const minAttr = this.getAttribute('min');
-        const maxAttr = this.getAttribute('max');
-        if (!minAttr || !maxAttr) {
-          this.shadowRoot.innerHTML = '';
-          return;
-        }
-
-        this.min = Number(minAttr);
-        this.max = Number(maxAttr);
-        if (isNaN(this.min) || isNaN(this.max)) {
-          this.shadowRoot.innerHTML = '';
-          return;
-        }
-
-        const valueMinAttr = this.getAttribute('valueMin');
-        const valueMaxAttr = this.getAttribute('valueMax');
-        const stepAttr = this.getAttribute('step');
-        const valueMinNum = Number(valueMinAttr);
-        const valueMaxNum = Number(valueMaxAttr);
-        const stepNum = Number(stepAttr);
-        this.valueMin = valueMinAttr && !isNaN(valueMinNum) ? valueMinNum : this.min;
-        this.valueMax = valueMaxAttr && !isNaN(valueMaxNum) ? valueMaxNum : this.max;
-        this.step = stepAttr && !isNaN(stepNum) ? stepNum : 1;
-        this.midpoint = (this.valueMax - this.valueMin) / 2 + this.valueMin;
-
-        const disabledAttr = this.getAttribute('disabled');
-        const setDisabled = disabledAttr === '' || disabledAttr === 'true';
-        this.disabled = setDisabled;
-
-        this._inputs.forEach((input, index) => {
-          this._setInputProperty(input, 'min', this.min);
-          this._setInputProperty(input, 'max', this.max);
-          this._setInputProperty(input, 'step', this.step);
-          this._setInputProperty(input, 'value', index === 0 ? this.valueMin : this.valueMax);
-
-          input.addEventListener('change', this._handleChange);
-          input.addEventListener('input', this._handleChange);
-          input.addEventListener('touchstart', this._handlePointerDown);
-        });
-
-        this._container.addEventListener('mousedown', this._handleSyntheticTrackClick);
-        this._container.addEventListener('click', this._handleSyntheticTrackClick);
-        this.addEventListener('mouseup', this._handlePointerUp);
-        this.addEventListener('touchend', this._handlePointerUp);
-
-        this.ready = true;
-        this.dispatchEvent(
-          new CustomEvent('ready', {
-            detail: {
-              min: this.min,
-              max: this.max,
-              step: this.step,
-              valueMin: this.valueMin,
-              valueMax: this.valueMax,
-            },
-          })
-        );
-      };
-
-      this._setInputProperty = (input, property, value) => {
-        input[property] = value;
-
-        const styleValue = property === 'value' ? input.valueAsNumber : value;
-        input.style.setProperty(`--${property}`, styleValue);
-
-        if (input.dataset.input === 'max' && property === 'value') {
-          input.dataset.atMin = value === this.min;
-        }
-      };
-
-      this._handleChange = (evt) => {
-        evt.stopPropagation();
-        const target = evt.target;
-        const isMinInput = target.dataset.input === 'min';
-        const inputValue = target.valueAsNumber;
-        const newValue = isMinInput ? Math.min(inputValue, this.valueMax) : Math.max(inputValue, this.valueMin);
-
-        this._setInputProperty(target, 'value', newValue);
-        const detail = {
-          min: this.min,
-          max: this.max,
-          step: this.step,
-          valueMin: isMinInput ? newValue : this.valueMin,
-          valueMax: isMinInput ? this.valueMax : newValue,
-        };
-        this.dispatchEvent(new CustomEvent('input', {detail}));
-
-        if (evt.type !== 'change') return;
-
-        this._setInputProperty(target, 'value', newValue);
-        isMinInput ? (this.valueMin = newValue) : (this.valueMax = newValue);
-      };
-
-      this._syntheticClickHandled = false;
-      this._handleSyntheticTrackClick = (evt) => {
-        if (evt.button !== 0) return;
-        evt.stopPropagation();
-        if (evt.type === 'click' && this._syntheticClickHandled) {
-          this._syntheticClickHandled = false;
-          return;
-        }
-        if (evt.type === 'mousedown') {
-          this._syntheticClickHandled = true;
-        }
-
-        const offsetX = evt.offsetX === 0 && evt.offsetY === 0 ? evt.layerX - evt.target.offsetLeft : evt.offsetX;
-        const offsetRatio = offsetX / evt.target.offsetWidth;
-        const midpointRatio = (this.midpoint - this.min) / (this.max - this.min);
-        const newValue = offsetRatio * (this.max - this.min) + this.min;
-        const isMinInput = offsetRatio < midpointRatio;
-        const target = isMinInput ? this._minInput : this._maxInput;
-
-        this._setInputProperty(target, 'value', newValue);
-        const detail = {
-          min: this.min,
-          max: this.max,
-          step: this.step,
-          valueMin: isMinInput ? target.valueAsNumber : this.valueMin,
-          valueMax: isMinInput ? this.valueMax : target.valueAsNumber,
-        };
-        this.dispatchEvent(new CustomEvent('input', {detail}));
-      };
-
-      this._handlePointerDown = (evt) => {
-        const path = evt.path ? evt.path : evt.composedPath();
-        if (!Array.isArray(path) || Array.length < 1) return;
-        const input = path[0];
-        input.focus();
-      };
-
-      this._handlePointerUp = (evt) => {
-        this.blur();
-        this._minInput.blur();
-        this._maxInput.blur();
-        this.valueMin = this._minInput.valueAsNumber;
-        this.valueMax = this._maxInput.valueAsNumber;
-      };
     }
 
     connectedCallback() {
       if (!this.isConnected) return;
-      this._init();
+      this.#init();
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -300,5 +156,149 @@ export default customElements.define(
         this.disabled = setDisabled;
       }
     }
+
+    #init = () => {
+      this._container = this.shadowRoot.querySelector('[part="container"]');
+      this._minInput = this.shadowRoot.querySelector('[data-input="min"]');
+      this._maxInput = this.shadowRoot.querySelector('[data-input="max"]');
+      this._inputs = [this._minInput, this._maxInput];
+
+      const minAttr = this.getAttribute('min');
+      const maxAttr = this.getAttribute('max');
+      if (!minAttr || !maxAttr) {
+        this.shadowRoot.innerHTML = '';
+        return;
+      }
+
+      this.min = Number(minAttr);
+      this.max = Number(maxAttr);
+      if (isNaN(this.min) || isNaN(this.max)) {
+        this.shadowRoot.innerHTML = '';
+        return;
+      }
+
+      const valueMinAttr = this.getAttribute('valueMin');
+      const valueMaxAttr = this.getAttribute('valueMax');
+      const stepAttr = this.getAttribute('step');
+      const valueMinNum = Number(valueMinAttr);
+      const valueMaxNum = Number(valueMaxAttr);
+      const stepNum = Number(stepAttr);
+      this.valueMin = valueMinAttr && !isNaN(valueMinNum) ? valueMinNum : this.min;
+      this.valueMax = valueMaxAttr && !isNaN(valueMaxNum) ? valueMaxNum : this.max;
+      this.step = stepAttr && !isNaN(stepNum) ? stepNum : 1;
+      this.midpoint = (this.valueMax - this.valueMin) / 2 + this.valueMin;
+
+      const disabledAttr = this.getAttribute('disabled');
+      const setDisabled = disabledAttr === '' || disabledAttr === 'true';
+      this.disabled = setDisabled;
+
+      this._inputs.forEach((input, index) => {
+        this.#setInputProperty(input, 'min', this.min);
+        this.#setInputProperty(input, 'max', this.max);
+        this.#setInputProperty(input, 'step', this.step);
+        this.#setInputProperty(input, 'value', index === 0 ? this.valueMin : this.valueMax);
+
+        input.addEventListener('change', this.#handleChange);
+        input.addEventListener('input', this.#handleChange);
+        input.addEventListener('touchstart', this.#handlePointerDown);
+      });
+
+      this._container.addEventListener('mousedown', this.#handleSyntheticTrackClick);
+      this._container.addEventListener('click', this.#handleSyntheticTrackClick);
+      this.addEventListener('mouseup', this.#handlePointerUp);
+      this.addEventListener('touchend', this.#handlePointerUp);
+
+      this.ready = true;
+      this.dispatchEvent(
+        new CustomEvent('ready', {
+          detail: {
+            min: this.min,
+            max: this.max,
+            step: this.step,
+            valueMin: this.valueMin,
+            valueMax: this.valueMax,
+          },
+        })
+      );
+    };
+
+    #setInputProperty = (input, property, value) => {
+      input[property] = value;
+
+      const styleValue = property === 'value' ? input.valueAsNumber : value;
+      input.style.setProperty(`--${property}`, styleValue);
+
+      if (input.dataset.input === 'max' && property === 'value') {
+        input.dataset.atMin = value === this.min;
+      }
+    };
+
+    #handleChange = (evt) => {
+      evt.stopPropagation();
+      const target = evt.target;
+      const isMinInput = target.dataset.input === 'min';
+      const inputValue = target.valueAsNumber;
+      const newValue = isMinInput ? Math.min(inputValue, this.valueMax) : Math.max(inputValue, this.valueMin);
+
+      this.#setInputProperty(target, 'value', newValue);
+      const detail = {
+        min: this.min,
+        max: this.max,
+        step: this.step,
+        valueMin: isMinInput ? newValue : this.valueMin,
+        valueMax: isMinInput ? this.valueMax : newValue,
+      };
+      this.dispatchEvent(new CustomEvent('input', {detail}));
+
+      if (evt.type !== 'change') return;
+
+      this.#setInputProperty(target, 'value', newValue);
+      isMinInput ? (this.valueMin = newValue) : (this.valueMax = newValue);
+    };
+
+    #syntheticClickHandled = false;
+    #handleSyntheticTrackClick = (evt) => {
+      if (evt.button !== 0) return;
+      evt.stopPropagation();
+      if (evt.type === 'click' && this.#syntheticClickHandled) {
+        this.#syntheticClickHandled = false;
+        return;
+      }
+      if (evt.type === 'mousedown') {
+        this.#syntheticClickHandled = true;
+      }
+
+      const offsetX = evt.offsetX === 0 && evt.offsetY === 0 ? evt.layerX - evt.target.offsetLeft : evt.offsetX;
+      const offsetRatio = offsetX / evt.target.offsetWidth;
+      const midpointRatio = (this.midpoint - this.min) / (this.max - this.min);
+      const newValue = offsetRatio * (this.max - this.min) + this.min;
+      const isMinInput = offsetRatio < midpointRatio;
+      const target = isMinInput ? this._minInput : this._maxInput;
+
+      this.#setInputProperty(target, 'value', newValue);
+      const detail = {
+        min: this.min,
+        max: this.max,
+        step: this.step,
+        valueMin: isMinInput ? target.valueAsNumber : this.valueMin,
+        valueMax: isMinInput ? this.valueMax : target.valueAsNumber,
+      };
+      this.dispatchEvent(new CustomEvent('input', {detail}));
+    };
+
+    #handlePointerDown = (evt) => {
+      const path = evt.path ? evt.path : evt.composedPath();
+      if (!Array.isArray(path) || Array.length < 1) return;
+      const input = path[0];
+      input.focus();
+    };
+
+    #handlePointerUp = (evt) => {
+      this.blur();
+      this._minInput.blur();
+      this._maxInput.blur();
+      this.valueMin = this._minInput.valueAsNumber;
+      this.valueMax = this._maxInput.valueAsNumber;
+    };
   }
 );
